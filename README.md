f43b59e523de3a22b93910508ca2c3daca0cd09e
jhjacobson <josh.jacobson@datadoghq.com>
2022-07-17 13:32:18 -0400

    Cleanup the processing and create output

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dc_voter_file_cleaning
+This helps ANCs find registered voters in their new district if they have the whole voter file and the boundaries of their ANC.
+
+Steps:
+1) Download this repo and add the voter file to "voter-list.xlsx".
+2) Use get_addresses_from_df(<dataframe>,<min_street_number>,<max_street_number,<street_name_lowercase>,<street_type>,<city_quadrant>,<odd_even_both>). There are examples in process.py for my ANC.
+3) Run the process and grab the file from "anc_voters.csv"
--- process.py
+++ process.py
@@ -10,40 +10,58 @@ list(df.columns)
 # print first row
 df.iloc[0]
 
-# Get NW data
-dfnw = df.loc[df['Street_Dir_Suffix'].str.lower() == 'nw']
+df_clean = df.drop(df[
+            (df['Street_Dir_Suffix'] == "**") | (df['Street_Name'].map(type)==float)
+            ].index) #remove bad data for Street Dir Suffix and
 
-# Make relevant columns lower case
-dfnw['Street_Type'] = dfnw['Street_Type'].apply(str.lower)
-dfnw['Street_Name'] = dfnw['Street_Name'].map(lambda x: x.lower() if isinstance(x, str) else x)
-dfnw['Street_Number'] = dfnw['Street_Number'].map(lambda x: x.rstrip(ascii_letters) if isinstance(x, str) else x)
-dfnw['Street_Number'] = dfnw['Street_Number'].astype("int")
+def streets_to_lowercase(street_name):
+    res = []
+    for index, c in enumerate(street_name):
+        if c.isalpha():
+            res.append(street_name[index].lower())
+        else:
+            res.append(street_name[index])
+    return ''.join(res)
 
-# Euclid St NW from 700 -> 999 (all)
-euclid = dfnw.loc[(dfnw['Street_Name'] == 'euclid') & (dfnw['Street_Type'] == 'st') & (dfnw['Street_Number'] >= 700) & (dfnw['Street_Number'] <= 999)]
+# Clean relevant columns for ordering and determining ANC district
+df_clean['Street_Type'] = df_clean['Street_Type'].apply(str.lower)
+df_clean['Street_Dir_Suffix'] = df_clean['Street_Dir_Suffix'].map(lambda x: x.lower() if isinstance(x, str) else x)
+df_clean['Street_Name'] = df_clean['Street_Name'].apply(streets_to_lowercase)
+df_clean['Street_Number'] = df['Street_Number'].map(lambda x: x.rstrip(ascii_letters) if isinstance(x, str) else x)
+df_clean['Street_Number'] = df['Street_Number'].astype("int")
 
-# Fairmont St NW from 700 -> 799 (evens)
-fairmount = dfnw.loc[(dfnw['Street_Name'] == 'fairmont') & (dfnw['Street_Type'] == 'st') & (dfnw['Street_Number'] >= 700) & (dfnw['Street_Number'] <= 799) & (dfnw['Street_Number']%2 == 0)]
+def get_addresses_from_df(df,min_street_num,max_street_num,street_name,street_type,quadrant,even_odd_all):
+    temp = df.loc[
+                    (df['Street_Name'] == street_name) & 
+                    (df['Street_Type'] == street_type) & 
+                    (df['Street_Number'] >= min_street_num) & 
+                    (df['Street_Number'] <= max_street_num) &
+                    (df['Street_Dir_Suffix'].str.lower() == quadrant)
+                ]
+    if even_odd_all == "even":
+        return temp.loc[(temp['Street_Number']%2 == 0)]
+    elif even_odd_all == "odd":
+        return temp.loc[(temp['Street_Number']%2 == 1)]
+    else:
+        return temp
 
-# Georgia Ave NW from 2600 -> 2699 (evens)
-georgia = dfnw.loc[(dfnw['Street_Name'] == 'georgia') & (dfnw['Street_Type'] == 'ave') & (dfnw['Street_Number'] >= 2600) & (dfnw['Street_Number'] <= 2699) & (dfnw['Street_Number']%2 == 0)]
 
-# Sherman Ave NW from 2251 - 2699 (odds)
-sherman = dfnw.loc[(dfnw['Street_Name'] == 'sherman') & (dfnw['Street_Type'] == 'ave') & (dfnw['Street_Number'] >= 2251) & (dfnw['Street_Number'] <= 2699) & (dfnw['Street_Number']%2 == 1)]
+euclid = get_addresses_from_df(df_clean,700,999,"euclid","st","nw","all")
+fairmont = get_addresses_from_df(df_clean,700,799,"fairmont","st","nw","even")
+georgia = get_addresses_from_df(df_clean,2600,2699,"georgia","ave","nw","even")
+sherman = get_addresses_from_df(df_clean,2251,2699,"sherman","ave","nw","odd")
+ninth = get_addresses_from_df(df_clean,2300,2600,"9th","st","nw","all")
+voters_df = pd.concat([euclid,fairmont,georgia,sherman,ninth], ignore_index=True, sort=False)
+voters_df = voters_df.sort_values(by=["Street_Dir_Suffix","Street_Name","Street_Type","Street_Number"], axis = 0)
 
-# 9th St NW from 2300 -> 2600 (all)
-ninth = dfnw.loc[((dfnw['Street_Name'] == '9th') | (dfnw['Street_Name'] == '9TH') | (dfnw['Street_Name'] == '9Th')) & (dfnw['Street_Type'] == 'st') & (dfnw['Street_Number'] >= 2300) & (dfnw['Street_Number'] <= 2600)]
+voters_df.to_csv("anc_voters.csv", index=False)
 
-# Voters
-voters_df = pd.concat([euclid,fairmount,georgia,sherman,ninth], ignore_index=True, sort=False)
 voters_df['Party'] = voters_df['Party'].apply(str.lower)
+
 
 # Howard Plaza Towers - 2251 Sherman Ave NW (juniors and seniors)
 howard = voters_df.loc[(voters_df['Street_Name'] == 'sherman') & (voters_df['Street_Number'] == 2251)].sort_values('Registration_Date')
-
 howard_new = howard.loc[howard.Registration_Date >= pd.Timestamp(2018,9,1)]
 
 # Trellis House - 2323 Sherman Ave NW
 trellis = voters_df.loc[(voters_df['Street_Name'] == 'sherman') & (voters_df['Street_Number'] == 2323)].sort_values('Registration_Date')
-
-voters_df['Party'].value_counts()
